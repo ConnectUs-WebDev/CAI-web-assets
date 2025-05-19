85b1947ab765c38cb5ba1202de93c986dbf9808f
ConnectUs-WebDev <webdev.connectus@gmail.com>
2025-05-19 23:56:21 +0800

    Update products.js

--- products.js
+++ products.js
@@ -8,5 +8,5 @@
   const brand = [["CAI: Social Media & Email Marketing - Brand Builder", "CAI: Social Media & Email Marketing - Brand Accelerator"], ["CAI: Social Media & Email Marketing - Brand Builder (Quarterly)", "CAI: Social Media & Email Marketing - Brand Accelerator (Quarterly)"]];
   const bk_setup = ["CAI: Bookkeeping - Basic Setup", "CAI: Bookkeeping - Standard Setup", "CAI: Bookkeeping - Advanced Setup"];
   
-  mnth_prods = mnth_prods.concat(cai_packages[0], bookkeeping[0], brand[0]);
-  qrt_prods = qrt_prods.concat(cai_packages[1], bookkeeping[1], brand[1]);
+  mnth_prods = mnth_prods.concat(bookkeeping[0], brand[0]);
+  qrt_prods = qrt_prods.concat(bookkeeping[1], brand[1]);
