e0f57e54de60b7a8bc1d6e273821af784f2e46f0
ConnectUs-WebDev <webdev.connectus@gmail.com>
2025-05-20 00:13:29 +0800

    Update products.js

--- products.js
+++ products.js
@@ -7,6 +7,7 @@
   const bookkeeping = [["CAI: Essential Bookkeeping", "CAI: Premium Bookkeeping"],["CAI: Essential Bookkeeping (Quarterly)", "CAI: Premium Bookkeeping (Quarterly)"]];
   const brand = [["CAI: Social Media & Email Marketing - Brand Builder", "CAI: Social Media & Email Marketing - Brand Accelerator"], ["CAI: Social Media & Email Marketing - Brand Builder (Quarterly)", "CAI: Social Media & Email Marketing - Brand Accelerator (Quarterly)"]];
   const bk_setup = ["CAI: Bookkeeping - Basic Setup", "CAI: Bookkeeping - Standard Setup", "CAI: Bookkeeping - Advanced Setup"];
-  
+
+// Group monthly and quarterly products for labeling
   mnth_prods = mnth_prods.concat(bookkeeping[0], brand[0]);
   qrt_prods = qrt_prods.concat(bookkeeping[1], brand[1]);
