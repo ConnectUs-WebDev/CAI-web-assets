c414b9ac1d0ac40ef66e86d83f7429696a71be51
Justin <jnabong.cms@gmail.com>
2025-10-23 00:46:13 +0800

    remove recurring CAI Packages

--- products.js
+++ products.js
@@ -1,13 +1,22 @@
 // Products sorted by payment period
-  let mnth_prods = [];
-  let qrt_prods = [];
+let mnth_prods = [];
+let qrt_prods = [];
 
-  // Products sorted by type
-  const cai_packages = [["CAI Platform - Essential", "CAI Platform - Growth", "CAI Platform - Premium"], ["CAI Platform - Essential  (Quarterly)", "CAI Platform - Growth (Quarterly)", "CAI Platform - Premium (Quarterly)"]];
-  const bookkeeping = [["CAI: Bookkeeping - Essential", "CAI: Bookkeeping - Premium"],["CAI: Bookkeeping - Essential (Quarterly)", "CAI: Bookkeeping - Premium (Quarterly)"]];
-  const brand = [["CAI: Social Media & Email Marketing - Brand Builder", "CAI: Social Media & Email Marketing - Brand Accelerator"], ["CAI: Social Media & Email Marketing - Brand Builder (Quarterly)", "CAI: Social Media & Email Marketing - Brand Accelerator (Quarterly)"]];
-  const bk_setup = ["CAI: Bookkeeping - Basic Setup", "CAI: Bookkeeping - Standard Setup", "CAI: Bookkeeping - Advanced Setup"];
+// Products sorted by type
+const cai_packages = [
+    ["CAI Platform - Essential", "CAI Platform - Growth", "CAI Platform - Premium"], 
+    ["CAI Platform - Essential  (Quarterly)", "CAI Platform - Growth (Quarterly)", "CAI Platform - Premium (Quarterly)"]
+];
+const bookkeeping = [
+    ["CAI: Bookkeeping - Essential", "CAI: Bookkeeping - Premium"],
+    ["CAI: Bookkeeping - Essential (Quarterly)", "CAI: Bookkeeping - Premium (Quarterly)"]
+];
+const brand = [
+    ["CAI: Social Media & Email Marketing - Brand Builder", "CAI: Social Media & Email Marketing - Brand Accelerator"], 
+    ["CAI: Social Media & Email Marketing - Brand Builder (Quarterly)", "CAI: Social Media & Email Marketing - Brand Accelerator (Quarterly)"]
+];
+const bk_setup = ["CAI: Bookkeeping - Basic Setup", "CAI: Bookkeeping - Standard Setup", "CAI: Bookkeeping - Advanced Setup"];
 
 // Group monthly and quarterly products for labeling
-  mnth_prods = mnth_prods.concat(cai_packages[0], bookkeeping[0], brand[0]);
-  qrt_prods = qrt_prods.concat(cai_packages[1], bookkeeping[1], brand[1]);
+mnth_prods = mnth_prods.concat(bookkeeping[0], brand[0]);
+qrt_prods = qrt_prods.concat(bookkeeping[1], brand[1]);
